cb03b9768c712c118198c14b038e0d9162c9824d
Darren Taylor <shout@darrenonthe.net>
2015-09-05 16:29:56 +0100

    Added Elocrypt Trait

---
+++ src/Dtisgodsson/Elocrypt/ElocryptTrait.php
@@ -0,0 +1,41 @@
+<?php namespace Dtisgodsson\Elocrypt;
+
+use Illuminate\Contracts\Encryption\DecryptException;
+use Crypt;
+
+trait ElocryptTrait {
+
+    public function __set($key, $value)
+    {
+        if(in_array($key, $this->encryptable))
+        {
+            $value = Crypt::encrypt($value);
+        }
+
+        parent::__set($key, $value);
+    }
+
+    public function __get($key)
+    {
+        $value = parent::__get($key);
+
+        if(!isset($this->encryptable))
+        {
+            return $value;
+        }
+
+        if(in_array($key, $this->encryptable))
+        {
+            try
+            {
+                return Crypt::decrypt($value);
+            }
+            catch(DecryptException $exception)
+            {
+                return $value;
+            }
+        }
+
+        return $value;
+    }
+}
